5a8b2c6bf751949fa5dabd6436fcb3c7093ef03e
Da Woon Lee <daniel.lee9415@gmail.com>
2018-03-19 16:08:49 -0400

    team tessel

--- teamTessel/ambient/ambient.js
+++ teamTessel/ambient/ambient.js
@@ -6,24 +6,34 @@ var ambient = ambientlib.use(tessel.port['A']);
 ambient.on('ready', function () {
  // Get points of light and sound data.
   setInterval( function () {
+    let sendMessage = false;
     ambient.getLightLevel( function(err, lightdata) {
       if (err) throw err;
       ambient.getSoundLevel( function(err, sounddata) {
         if (err) throw err;
         console.log("Light level:", lightdata.toFixed(8), " ", "Sound Level:", sounddata.toFixed(8));
-        if (sounddata > .1) {
-          console.log('I hear you');
-        }
 
-        if(lightdata < .01) {
-          console.log('its dark in here')
-        }
 
       });
     });
   }, 1000);
    // The readings will happen every .5 seconds
 });
+
+ambient.on('light-trigger', function (lightTriggerValue) {
+   if(lightdata < .5) {
+    console.log('its dark in here')
+    // clearInterval(setInterval)
+  }
+});
+
+ambient.on('sound-trigger', function (soundTriggerValue) {
+  if (sounddata > .1) {
+
+      console.log('I hear you');
+
+  }
+})
 
 ambient.on('error', function (err) {
   console.log(err);
