378ac02ddb573e99a257ec073ab79634416de2a3
Ian Dewsbury <ianmichaeld@gmail.com>
2018-03-19 16:23:04 -0400

    slack module take 2

--- teamTessel/ambient/slack.js
+++ teamTessel/ambient/slack.js
@@ -3,21 +3,21 @@ const slackWebhookUrl = 'https://hooks.slack.com/services/T024FPYBQ/B9RT7LYEM/Ex
 
 const spamSlack = new IncomingWebhook(slackWebhookUrl);
 
-spamSlack.send('Add this line to your ambient.js: const Slack = require("./slack.js")', (error, resp) => {
-  if (error) {
-    return console.error(error);
-  }
-  console.log('Notification sent');
-});
+// spamSlack.send('Add this line to your ambient.js: const Slack = require("./slack.js")', (error, resp) => {
+//   if (error) {
+//     return console.error(error);
+//   }
+//   console.log('Notification sent');
+// });
 
-// const Slack = function(message) {
-//   spamSlack.send( message, (error, resp) => {
-//     if (error) {
-//       return console.error(error);
-//     }
-//     console.log('Notification sent');
-// })}
+const Slack = function(message) {
+  spamSlack.send( message, (error, resp) => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log('Notification sent');
+})}
 
-// module.exports = Slack;
+module.exports = Slack;
 
-// const Slack = require('./slack.js')
+const Slack = require('./slack.js')
